d5ff8e8d86edc151418ddf3032900f630755e9a1
thfield <thfield@gmail.com>
2013-12-08 12:53:23 -0800

    functions working

--- js/app.js
+++ js/app.js
@@ -3,112 +3,137 @@
 $(document).foundation();
 
 $(document).ready(function () {
-    var activ = '';
-    var z1 = $('#zone1');
-    var z2 = $('#zone2');
-    var z3 = $('#zone3');
+    var activeString = '';
+    var activeNum;
+    var l = [$('#line0'), $('#line1'), $('#line2'), $('#line3')];
+    var a = $('#lineA');
     var stack = [];
-    var opCase;
     var answer;
 
-    // when button is clicked, pass data-val for button to a function 
-    // {then display variable z1 in zone1 div} make this a function
+    //test output function
+    function testOutput(str) {
+        $('#testing').text(str);
+    };
+    $(document).on('click', '#posneg', function () {
+        testOutput('str:' + activeString + ' num:' + activeNum + ' s:' + stack[0] + ' ' + stack [1] + ' ' + stack[2] + ' ' + stack[3]);
+    });
+
+    // when number button is clicked, pass data-val to function takeIn()
     $(document).on('click', '.number', function () {
         x = $(this).data('val');
-        //Number(x);
         takeIn(x);
     });
 
-    // function appends num to the active operand
+    // function appends numStr to the active operand string
     function takeIn(numStr) {
-        activ += numStr.toString();
-        displayInZone(activ);
+        activeString += numStr.toString();
+        displayActiv();
     };
 
-    // displays foo in the zone(x) div
-    function displayInZone(foo) {
-        z1.text(foo);
+    function strToNum() {
+        if (activeString !== ''){
+            activeNum = Number(activeString);
+        };
     };
 
-    // backspace
-    $(document).on('click', '#bksp', function () {
-        last = activ.length - 1;
-        activ = activ.substring(0, last);
-        displayInZone(activ);
+    // click on enter button
+    $(document).on('click', '#enter', function () {
+        if (activeString === ''){
+            activeNum = stack[0];            
+        } else {
+        strToNum();
+        }
+        stack.unshift(activeNum);
+        displayStack();
+        clearActive();
     });
 
-    // clear
-    function clear() {
-        activ = '';
-        displayInZone(activ);
+    function displayActiv() {
+        a.text(activeString);
+    };
+
+    function displayStack() {
+        for (var i = 0; i < 4; i += 1) {
+            l[i].text('');
+            l[i].text(stack[i]);
+        };
+    };
+
+    // click backspace button
+    $(document).on('click', '#bksp', function () {
+        if (activeString === '') {
+            stack.shift();
+            displayStack();
+        } else {
+            last = activeString.length - 1;
+            activeString = activeString.substring(0, last);
+            displayActiv();
+        };
+    });
+
+    // clear active
+    function clearActive() {
+        activeString = '';
+        activeNum = undefined;
+        displayActiv();
     };
 
     // click clear button
     $(document).on('click', '#clear', function () {
-        clear();
+        stack.length = 0;
+        displayStack();
+        clearActive();
     });
 
-    // when operator button is pressed, make activ a number, move to end of stack[], display in zone2, clear zone1
-    function opPress() {
-        foo = Number(activ);
-        stack.push(foo);
-        z2.text(stack[stack.length - 1]);
-
+    // TODO change y to unshift only if activeNum exists
+    function opPress(opCase) {
+        strToNum();
+        if (!isNaN(activeNum)) {
+            stack.unshift(activeNum);
+        };
+        if ( typeof stack[1] == 'undefined') {
+            return;
+        } else{
+            y = stack.shift();
+            x = stack.shift();
+            switch (opCase) {
+            case "a":
+                answer = x + y;
+                break;
+            case "s":
+                answer = x - y;
+                break;
+            case "m":
+                answer = x * y;
+                break;
+            case "d":
+                answer = x / y;
+                break;
+            }
+            clearActive();
+            stack.unshift(answer);
+            displayStack();
+        };
     };
 
-    // addition
+    // addition click
     $(document).on('click', '#add', function () {
-        opPress();
-        clear();
-        z3.text('+');
-        opCase = 'a';
+        opPress('a');
     });
 
-    // subtraction
+    // subtraction click
     $(document).on('click', '#subtract', function () {
-        opPress();
-        clear();
-        z3.text('-');
-        opCase = 's';
+        opPress('s');
     });
 
-    // multiplication
+    // multiplication click
     $(document).on('click', '#multiply', function () {
-        opPress();
-        clear();
-        z3.text('×');
-        opCase = 'm';
+        opPress('m');
     });
 
-    // division
+    // division click
     $(document).on('click', '#divide', function () {
-        opPress();
-        clear();
-        z3.text('÷');
-        opCase = 'd';
+        opPress('d');
     });
 
-    // equals
-    $(document).on('click', '#equals', function () {
-        x = stack.pop();
-        y = Number(activ);
-        switch (opCase) {
-        case "a":
-            answer = x + y;
-            break;
-        case "s":
-            answer = x - y;
-            break;
-        case "m":
-            answer = x * y;
-            break;
-        case "d":
-            answer = x / y;
-            break;
-        }
-        z1.text(answer);
-        z2.text('');
-        z3.text('');
-        activ=answer;
-    });
 });
