d3ac0df404a950ffa3a03cae51c2c67d3f3eaa99
thfield <thfield@gmail.com>
2013-12-18 13:40:44 -0800

    typewriter arrangement fixed

--- js/numgrid.js
+++ js/numgrid.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
-    $('li[data-val="9"]').insertAfter('li[data-val="8"]');
-    $('li[data-val="7"]').insertBefore('li[data-val="8"]');
+    function reorder() {
+        $('li[data-val="9"]').insertAfter('li[data-val="8"]');
+        $('li[data-val="7"]').insertBefore('li[data-val="8"]');
 
-    $('li[data-val="6"]').insertAfter('li[data-val="5"]');
-    $('li[data-val="4"]').insertBefore('li[data-val="5"]');
+        $('li[data-val="6"]').insertAfter('li[data-val="5"]');
+        $('li[data-val="4"]').insertBefore('li[data-val="5"]');
 
-    $('li[data-val="3"]').insertAfter('li[data-val="2"]');
-    $('li[data-val="1"]').insertBefore('li[data-val="2"]');
+        $('li[data-val="3"]').insertAfter('li[data-val="2"]');
+        $('li[data-val="1"]').insertBefore('li[data-val="2"]');
+    };
 
     // http://stackoverflow.com/questions/3225346/javascript-to-turn-an-unordered-list-into-multiple-columns
-    jQuery(function ($) {
+    function rearrange() {
         var size = 3,
             $nums = $("#numberpad"),
             $lis = $nums.children().filter(':gt(' + (size - 1) + ')'),
@@ -19,11 +21,29 @@ $(document).ready(function () {
         for (; i < loop; i = i + 1) {
             $nums = $("<ul />").attr('class', 'buttongroup nums').append($lis.slice(i * size, (i * size) + 4)).insertAfter($nums);
         }
-    });
-
+    };
+    
+    function typeOrder() {
+        $('#operators').insertBefore('#nums');
+    };
+    
+    function untype() {
+        $('#operators').insertAfter('#nums');
+    };
+    
     // http://stackoverflow.com/questions/7846980/how-do-i-switch-my-css-stylesheet-using-jquery
-    $("#csspick li a").click(function() { 
-		$("link").attr("href",$(this).attr('rel'));
-		return false;
-	});
+    $("#csspick li a").click(function () {
+        reorder();
+        rearrange();
+        untype();
+        $("link").attr("href", $(this).attr('rel'));
+        return false;
+    });
+    
+     $('a[rel="stylesheets/typewriter.css"]').click(function () {
+        typeOrder()
+        return false;
+    });
+    
+
 });
